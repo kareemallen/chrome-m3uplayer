977504b75225a0a33bf337657053187d5d56d358
Kareem <Absolut901@yahoo.com>
2014-10-09 19:59:52 -0500

    Got the function to get the file to execute

--- background.js
+++ background.js
@@ -4,13 +4,28 @@
  * and open the template in the editor.
  */
 
-console.log ("Background.js loaded");
+console.log("Background.js loaded");
 
-function myAlert() {
-    console.log("My alert");
-}
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('open-button').addEventListener('click', function() {
-        chrome.fileSystem.chooseEntry({ type: "openFile" }, myAlert);
+    document.getElementById('open-button').addEventListener('click', function () {
+        chrome.fileSystem.chooseEntry({type: "openFile"}, getFile);
     });
 });
+
+function getFile(readOnlyEntry) {
+    if (!readOnlyEntry) {
+        console.log("User did not choose a file");
+        return;
+    }
+    readOnlyEntry.file(function (file) {
+        var reader = new FileReader();
+//        reader.onerror = errorHandler;
+        reader.onloadend = function (e) {
+            console.log(e.target.result);
+        };
+        reader.readAsText(file);
+//        console.log(file);
+//        var playlist = M3U.parse(file);
+//        console.log(playlist);
+    });
+}
